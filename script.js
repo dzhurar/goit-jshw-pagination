7125b90f6d6437720eccdcb766f4a0f6026c5dd1
dzhurar <142822867+dzhurar@users.noreply.github.com>
2025-04-23 08:55:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const loadBtn = document.getElementById('loadMoreBtn');
 let page = 1;
 async function getImgs(pageNumber = 1){
   try {
-    const imagesData = await fetch(`https://pixabay.com/api?key=49866368-09b26b9b0dd4ad79533ce9094&editors_choice=true&orientation=horizontal&page=${pageNumber}&per_page=10`)
+    const imagesData = await fetch(`https://pixabay.com/api/?key=49866368-09b26b9b0dd4ad79533ce9094&editors_choice=true&orientation=horizontal&page=${pageNumber}&per_page=10`)
     const { hits } = await imagesData.json();
     renderImgs(hits);
   } catch (error) {
